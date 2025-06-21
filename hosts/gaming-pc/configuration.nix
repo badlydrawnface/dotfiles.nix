284467936fe6996879fd659bee23827eda7af08d
badlydrawnface <bdface@tuta.io>
2025-06-20 23:58:34 -0400

    disable waydroid on gaming-pc due to build issue

--- hosts/gaming-pc/configuration.nix
+++ hosts/gaming-pc/configuration.nix
@@ -35,7 +35,7 @@
   virtualisation = {
     docker.enable = true;
     podman.enable = true;
-    waydroid.enable = true;
+    #waydroid.enable = true;
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
   };
