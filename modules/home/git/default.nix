06b944f65d8b0eb06af73e1a13d7f86a17cac2e1
badlydrawnface <bdface@tuta.io>
2025-06-20 23:48:19 -0400

    forgor to change my gitconfig email 🤦‍♂️

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,4 +1,10 @@
-{ pkgs, lib, config, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
 
   options = {
     git.enable = lib.mkEnableOption "enable git and configure gitconfig";
@@ -7,7 +13,7 @@
   config = lib.mkIf config.git.enable {
     programs.git = {
       enable = true;
-      userEmail = "bdface@tuta.io";
+      userEmail = "bdface@proton.me";
       userName = "badlydrawnface";
       aliases = {
         hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
