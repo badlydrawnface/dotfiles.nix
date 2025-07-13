a56f55ac15174bc78cad0b65861bc225efefbf7a
badlydrawnface <bdface@proton.me>
2025-07-13 16:14:16 -0400

    remove wlsunset systemd module

--- modules/home/desktops/common/wlsunset/default.nix
+++ modules/home/desktops/common/wlsunset/default.nix
@@ -20,28 +20,6 @@
       temperature.night = 3500;
     };
 
-    systemd.user.services = lib.mkForce {
-      wlsunset = {
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
-
-        Unit = {
-          Description = "Day/night gamma adjustments for Wayland";
-          Documentation = [ "man:wlsunset(1)" ];
-          After = [ "graphical-session.target" ];
-        };
-
-        Service = {
-          Type = "exec";
-          ExecStart = "${pkgs.wlsunset}/bin/wlsunset -t 2500 -l 40.6 -L -75.4";
-          ExecCondition = "${pkgs.systemd}/lib/systemd/systemd-xdg-autostart-condition \"Hyprland\" \"\" ";
-          Restart = "on-failure";
-          Slice = "background-graphical.slice";
-        };
-      };
-    };
-
     home.packages = with pkgs; [
       wlsunset
     ];
