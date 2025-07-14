fb4925b8ca12fd5784256bb412f733aeb1c4d0eb
badlydrawnface <bdface@proton.me>
2025-07-13 20:25:48 -0400

    adjust brave config (still doesn't work), remove zen folder

--- modules/home/browsers/brave/default.nix
+++ modules/home/browsers/brave/default.nix
@@ -1,17 +1,25 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   options = {
-    browsers.brave.enable = lib.mkEnableOption "Enable and configure Brave/Chromium";
+    browsers.brave.enable = lib.mkEnableOption "Enable and configure Brave";
   };
 
   config = lib.mkIf config.browsers.brave.enable {
     programs.chromium = {
       enable = true;
+      package = pkgs.brave;
       extensions = [
-        "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock
-        "gebbhagfogifgggkldgodflihgfeippi" # returnyoutubedislike
-        "ponfpcnoihfmfllpaingbgckeeldkhle" # enhancer for youtube
+        { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # proton pass
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # sponsorblock
+        { id = "gebbhagfogifgggkldgodflihgfeippi"; } # returnyoutubedislike
+        { id = "ponfpcnoihfmfllpaingbgckeeldkhle"; } # enhancer for youtube
       ];
     };
+    catppuccin.chromium.enable = true;
   };
 }
